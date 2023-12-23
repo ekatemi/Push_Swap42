9de2a485d8cc9a76d718870a2fe2edc1ffd33467
Ekaterina Mikhaylova <emikhayl@cbr10s4.42barcelona.com>
2023-12-23 16:20:09 +0100

    check input

--- main.c
+++ main.c
@@ -14,24 +14,37 @@
 #include <stdlib.h>
 
 //-2,147,483,648 to 2,147,483,647
+int ft_isnum(char *str)
+{
+    if (*str == '-')
+        str++;
+    while(*str)
+    {
+        if (*str < '0' || *str > '9')
+            return(0);
+        str++;
+    }
+    return (1);
+}
+
+int ft_isint(long num)
+{
+    
+}
+
+
 
 int main(int  argc, char  *argv[])
 {
     int i;
-    
+
     i = 1;
     if (argc < 2)
         return (1);
-    while (argc > i) 
+    while (i < argc) 
     {
-        char *str = argv[i];  // Replace this with your actual char array
-        char *endptr;  // Pointer to the first character not converted
-        long num = strtol(str, &endptr, 10);  // C
-        if (*endptr != '\0') {
-            printf("Error: Invalid number format: %s\n", str);
-            return 1;  // Exit with an error code
-        }
-        printf("%ld\n", num);
+        char *str = argv[i];
+        
         i++;
     }
     return (0);
